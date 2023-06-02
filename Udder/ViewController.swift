88be423d15815997c9ef790fb98ccd33c237388a
Ken Brown <6101370+ken-brown@users.noreply.github.com>
2023-06-02 16:33:31 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ken Brown on 6/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
